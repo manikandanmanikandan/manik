09771548d69494255746cfae94ffb5e717621a52
manikandanmanikandan <manipanneer1996@gmail.com>
2016-12-22 14:10:30 +0530

    Add files via upload

--- pos.c
+++ pos.c
@@ -1,15 +1,14 @@
-#include<stdio.h
+#include<stdio.h>
 #include<conio.h>
-void main() 
-{ 
-int n;
-printf("enter the number:"); 
-scanf("%d",&n);
-if(n>=0)
-{
-printf(" positive number :%d",n);
-} else
-{  
-printf("negative number :%d",n);
-} 
-}
+ void main()
+ {
+ int n;
+ printf("enter the number:");
+ scanf("%d",&n);
+ if(n>=0)
+ {
+ printf(" positive number :%d",n);
+ }
+ else
+ {  printf("negative number :%d",n);
+} }
