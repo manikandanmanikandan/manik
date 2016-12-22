aec52075835fd9dd16ac3148008da63a90ff7b93
manikandanmanikandan <manipanneer1996@gmail.com>
2016-12-22 13:48:20 +0530

    Create PON.C

---
+++ PON.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter the number:");
+scanf("%d",&n);
+if(n<=0)
+{
+printf(" positive number :%d",n);
+}
+else
+{
+printf("negative number :%d",n);
+}
+}
