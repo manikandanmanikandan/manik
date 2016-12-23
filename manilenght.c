cac060ca32cc02ce22ba6646e4ea94ddf944d4d1
manikandanmanikandan <manipanneer1996@gmail.com>
2016-12-23 13:55:59 +0530

    Add files via upload

---
+++ manilenght.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,count=0;
+printf("enter the string ");
+scanf("%d",&i);
+while(n)
+{
+    i=i/10;
+    count++;
+}
+printf("count :%d",count);
+}
