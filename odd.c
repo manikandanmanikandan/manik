602e7a94a2325faf100ad281e8af96c9294ce156
manikandanmanikandan <manipanneer1996@gmail.com>
2016-12-23 09:45:22 +0530

    Add files via upload

---
+++ odd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter the values:");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("is a even");
+}
+else
+{
+printf("is a odd");
+}
+}
