6f065346b995dc7fa944e4b2dfbccbc8092beff4
manikandanmanikandan <manipanneer1996@gmail.com>
2016-12-23 09:48:28 +0530

    Add files via upload

---
+++ vowelornot.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char n;
+printf("enter the values:");
+scanf("%c",&n);
+if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u'||n=='A'||n=='E'||n=='I'||n=='O')
+{
+printf("is a vowel");
+}
+else
+{
+printf("is a conson");
+}
+}
+
