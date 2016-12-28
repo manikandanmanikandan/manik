117edd241673963ee2a18acd369e09220bc36d25
manikandanmanikandan <manipanneer1996@gmail.com>
2016-12-28 11:01:13 +0530

    Add files via upload

---
+++ enode the string.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+char* encode(char* str) {
+   int i = 0;
+   while (str[i] != '\0')
+    {
+      str[i] = str[i]-30;
+   }
+   return (str);
+}
+void main() {
+
+   char *str;
+   printf("\nEnter the String to be Encode : ");
+   gets(str);
+   str = encode(str);
+   printf("\nEncoded String : %s", str);
+   getch();
+}
