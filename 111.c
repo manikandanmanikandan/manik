6818a0abeaaef1852387e295c3a72baff2284940
manikandanmanikandan <manipanneer1996@gmail.com>
2016-12-30 13:57:23 +0530

    Add files via upload

---
+++ 111.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,n,j,a;
+scanf("%d",&n);
+for(a=0;a<n;a++)
+{
+for(i=0;i<n;i++)
+{
+printf("\n");
+for(j=i;j<n;j++)
+    printf("*");
+}
+}}
+
