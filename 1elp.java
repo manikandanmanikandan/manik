373f7b2009b4a7e572982b43de1a4f81323016c6
manikandanmanikandan <manipanneer1996@gmail.com>
2017-06-25 11:03:40 +0530

    Add files via upload

---
+++ 1elp.java
@@ -0,0 +1,39 @@
+package test;
+
+import java.util.Scanner;
+
+public class elep {
+	public static void main(String []args)
+	{
+		Scanner s=new Scanner(System.in);
+		int m=s.nextInt();
+		int a[]=new int[m];
+		int w=s.nextInt();
+		for(int i=0;i<a.length;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		
+		int sum=0,count=0;
+		for(int i=0;i<a.length;i++)
+		{
+			
+			if(sum<w)
+			{
+				sum=a[i]+sum;
+				
+			if(sum<w)
+			{
+				
+			
+			count++;
+			
+			}
+			}
+		}
+		System.out.println(count);
+		
+		
+	}
+
+}
