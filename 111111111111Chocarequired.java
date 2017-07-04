18bdfef5847524f833c252082d627b6814131812
manikandanmanikandan <manipanneer1996@gmail.com>
2017-07-04 15:12:10 +0530

    Update 111111111111Chocarequired.java

--- 111111111111Chocarequired.java
+++ 111111111111Chocarequired.java
@@ -8,7 +8,7 @@ public class childrencandy {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt();
-		int a[]=new int[5];
+		int a[]=new int[n];
 		for(int i=0;i<n;i++)
 		{
 			a[i]=s.nextInt();
